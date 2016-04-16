5e3809478d089287182267b687e225b1e0aae844
jsoni2 <jakzi7005@gmail.com>
2016-04-16 16:45:56 -0700

    comment:location for 09

--- src/common/RoverLocations.java
+++ src/common/RoverLocations.java
@@ -36,6 +36,7 @@ public class RoverLocations {
 		// test rovers
 		roverHash.put(RoverName.ROVER_00, new Coord(4,20));
 		roverHash.put(RoverName.ROVER_99, new Coord(4,7));
+		// location for ROVER09
 		roverHash.put(RoverName.ROVER_09, new Coord(10,10));
 
 	}
