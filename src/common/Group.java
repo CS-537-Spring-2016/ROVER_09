db52650bdce399d05613b25381d96ff83128ef27
shay <shay.nguyen@gmail.com>
2016-05-17 13:45:11 -0700

    fix merge conflict, delete previous merge

--- src/common/Group.java
+++ src/common/Group.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package common;
 
 /**
@@ -18,19 +17,3 @@ public class Group {
 		this.port = port;
 	}
 }
-=======
-package common;
-
-public class Group {
-
-	public String ip;
-	public int port;
-	public String group;
-
-	public Group(String group, String ip, int port) {
-		this.group = group;
-		this.ip = ip;
-		this.port = port;
-	}
-}
->>>>>>> refs/remotes/origin/Janak
